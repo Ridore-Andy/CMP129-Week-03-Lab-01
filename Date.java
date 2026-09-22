cf1d5b08731caef7d99c58e417e76a24fa502c79
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-22 14:41:30 -0400

    Complete Week 3 lab 1

--- Date.java
+++ Date.java
@@ -1,3 +1,29 @@
-public class Date {
-    
+public class Date 
+{
+    int month = 0;
+    int day = 0;
+    int year = 0;
+
+    //
+    public int getMonth(int month)
+    {return month;}
+    public void setMonth(int month)
+    {this.month = month;}
+
+    public int getDay(int day)
+    {return day;}
+    public void setDay(int day)
+    {this.day = day;}
+
+    public int getYear(int year)
+    {return year;}
+    public void setYear(int year)
+    {this.month = year;}
+
+    public void theDate()
+    {
+        System.out.println(month+"/"+day+"/"+year);
+        System.out.println("month "+day+","+year);
+        System.out.println(day+" month "+year);
+    }    
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,38 @@
-public class DateTest {
+import java.util.Scanner;
+public class DateTest 
+{
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        Date d1 = new Date();
+
+        System.out.println("Enter the month.");
+        int theMonth = keyboard.nextInt();
+        while(theMonth < 1 || theMonth > 12)
+        {
+            System.out.println("That is not a month");
+            System.out.println("Enter the month.");
+            theMonth = keyboard.nextInt();
+        }
+        d1.setMonth(theMonth);
+
+        System.out.println("Enter the day.");
+        int theDay = keyboard.nextInt();
+        while(theDay < 1 || theDay > 31)
+        {
+            System.out.println("There are not that many days in a month");
+            System.out.println("Enter the day.");
+            theDay = keyboard.nextInt();
+        }
+        d1.setDay(theDay);
+
+        System.out.println("Enter the year.");
+        d1.setYear(keyboard.nextInt());
+
+        d1.theDate();
+
+        keyboard.close();
+    }
     
 }
