b34218851841be04ade9692611e4f9f520996be8
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-22 20:13:15 -0400

    Complete Week 3 lab 1

--- Employee.java
+++ Employee.java
@@ -1,3 +1,62 @@
-public class Employee {
+public class Employee 
+{
+    private String name;
+    private int idNumber;
+    private String department;
+    private String position;
+
+    //constuctor 1
+    public Employee(String name, int idNumber, String department, String position)
+    {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.department = department;
+        this.position = position;
+    }
+
+    //constructor 2
+    public Employee(String name, int idNumber)
+    {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.department = "";
+        this.position = "";
+    }
+    //constructor 3
+    public Employee()
+    {
+        this.name = "";
+        this.idNumber = 0;
+        this.department = "";
+        this.position = "";
+    }
+
+
+    public String getName()
+    {return name;}
+    public void setName(String name)
+    {this.name = name;}
+
+    public int getIdNumber()
+    {return idNumber;}
+    public void setIdNumber(int idNumber)
+    {this.idNumber = idNumber;}
+
+    public String getDepartment()
+    {return department;}
+    public void setDepartment(String department)
+    {this.department = department;}
+
+    public String getPosition()
+    {return position;}
+    public void setPosition(String position)
+    {this.position = position;}
     
+
+    //display
+    public void displayInfo()
+    {
+        System.out.printf("\n%-20s %-20s %-20s %-20s%n","Name","ID Number","Department","Position");
+        System.out.printf("%-20s %-20s %-20s %-20s%n",name, idNumber, department, position);
+    }
 }
--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,14 @@
-public class EmployeeTest {
+public class EmployeeTest 
+{
+    public static void main(String[] args) 
+    {
+        Employee e1 = new Employee("Susan Meyers",47899,"Accounting","Vice President");
+        Employee e2 = new Employee("Mark Jones",39119,"IT","Programmer");
+        Employee e3 = new Employee("Joy Rogers",81774,"Manufacturing","Engineer");
+
+        e1.displayInfo();
+        e2.displayInfo();
+        e3.displayInfo();
+    }
     
 }
